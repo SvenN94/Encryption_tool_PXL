af0b27d5c8b80aff35b75aff92e7ef9e12af7757
ItzBob <51436202+SvenN94@users.noreply.github.com>
2023-03-13 10:13:12 +0100

    AES layout completed

---
+++ Encryptie_Tool/Encryptie_Tool/AESWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Encryptie_Tool
+{
+    /// <summary>
+    /// Interaction logic for AESWindow.xaml
+    /// </summary>
+    public partial class AESWindow : Window
+    {
+        public AESWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- Encryptie_Tool/Encryptie_Tool/MainWindow.xaml.cs
+++ Encryptie_Tool/Encryptie_Tool/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,43 @@ namespace Encryptie_Tool
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void AesWindowMenu_Click(object sender, RoutedEventArgs e)
+        {
+            AESWindow wpf = new AESWindow();
+            wpf.Show();
+        }
+
+        private void RsaWindowManu_Click(object sender, RoutedEventArgs e)
+        {
+            RSAWindow wpf = new RSAWindow();
+            wpf.Show();
+        }
+
+        private void BtnGenAes_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnGenRsa_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        string folderAes;
+        
+        private void AesFolderMenu_Click(object sender, RoutedEventArgs e)
+        {             
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.ValidateNames = false;
+            ofd.CheckFileExists = false;
+            ofd.CheckPathExists = true;
+            ofd.FileName = TxtFileName.Text;
+        }
+        string folderRsa;
+        private void RsaFolderMenu_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
