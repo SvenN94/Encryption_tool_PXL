539ffbbc6fb8f980d0cc1655c06eadbfcdccdc36
ItzBob <51436202+SvenN94@users.noreply.github.com>
2023-04-14 15:18:02 +0200

    folder path wordt doorgegeven aan volgende schermen

--- Encryptie_Tool/Encryptie_Tool/MainWindow.xaml.cs
+++ Encryptie_Tool/Encryptie_Tool/MainWindow.xaml.cs
@@ -2,17 +2,21 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using OpenFileDialog = System.Windows.Forms.OpenFileDialog;
+using SaveFileDialog = System.Windows.Forms.SaveFileDialog;
 
 namespace Encryptie_Tool
 {
@@ -29,37 +33,59 @@ namespace Encryptie_Tool
         private void AesWindowMenu_Click(object sender, RoutedEventArgs e)
         {
             AESWindow wpf = new AESWindow();
-            wpf.Show();
+            wpf.folder = folderAes;
+            wpf.ShowDialog();
         }
 
         private void RsaWindowManu_Click(object sender, RoutedEventArgs e)
         {
             RSAWindow wpf = new RSAWindow();
-            wpf.Show();
+            wpf.folder = folderRsa;
+            wpf.ShowDialog();
         }
 
         private void BtnGenAes_Click(object sender, RoutedEventArgs e)
         {
+            using ( Aes aesobj = Aes.Create())
+            {
+                aesobj.GenerateKey();
+                string keyBase64 = Convert.ToBase64String(aesobj.Key);
+                aesobj.GenerateIV();
+                string IvBase64 = Convert.ToBase64String(aesobj.IV);
+            }
+            SaveFileDialog dlg = new SaveFileDialog();
+            dlg.InitialDirectory = folderAes;
+            dlg.Filter = "Text file (*.txt)|*.txt";
+            dlg.ShowDialog();
+
 
         }
 
         private void BtnGenRsa_Click(object sender, RoutedEventArgs e)
         {
 
+
         }
         string folderAes;
         
         private void AesFolderMenu_Click(object sender, RoutedEventArgs e)
-        {             
-            OpenFileDialog ofd = new OpenFileDialog();
-            ofd.ValidateNames = false;
-            ofd.CheckFileExists = false;
-            ofd.CheckPathExists = true;
-            ofd.FileName = TxtFileName.Text;
+        {
+            FolderBrowserDialog fbp = new FolderBrowserDialog();
+            DialogResult result = fbp.ShowDialog();
+            if (result == System.Windows.Forms.DialogResult.OK)
+            {
+                folderAes = fbp.SelectedPath;
+            }
         }
         string folderRsa;
         private void RsaFolderMenu_Click(object sender, RoutedEventArgs e)
         {
+            FolderBrowserDialog fbp = new FolderBrowserDialog();
+            DialogResult result = fbp.ShowDialog();
+            if (result == System.Windows.Forms.DialogResult.OK)
+            {
+                folderRsa = fbp.SelectedPath;
+            }
 
         }
     }
--- Encryptie_Tool/Encryptie_Tool/RSAWindow.xaml.cs
+++ Encryptie_Tool/Encryptie_Tool/RSAWindow.xaml.cs
@@ -7,6 +7,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -19,9 +20,54 @@ namespace Encryptie_Tool
     /// </summary>
     public partial class RSAWindow : Window
     {
+        public string folder;
         public RSAWindow()
         {
             InitializeComponent();
+        }
+        private void AesWindowMenu_Click(object sender, RoutedEventArgs e)
+        {
+            AESWindow wpf = new AESWindow();
+            wpf.Show();
+        }
+
+        private void RsaWindowManu_Click(object sender, RoutedEventArgs e)
+        {
+            RSAWindow wpf = new RSAWindow();
+            wpf.Show();
+        }
+
+        private void BtnGenAes_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnGenRsa_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        string folderAes;
+
+        private void AesFolderMenu_Click(object sender, RoutedEventArgs e)
+        {
+            //OpenFileDialog ofd = new OpenFileDialog();
+            //ofd.ValidateNames = false;
+            //ofd.CheckFileExists = false;
+            //ofd.CheckPathExists = true;
+            //ofd.FileName = TxtFileName.Text;
+
+
+        }
+        string folderRsa;
+        private void RsaFolderMenu_Click(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Forms.FolderBrowserDialog fbp = new System.Windows.Forms.FolderBrowserDialog();
+            System.Windows.Forms.DialogResult result = fbp.ShowDialog();
+            if (result == System.Windows.Forms.DialogResult.OK)
+            {
+                folderRsa = fbp.SelectedPath;
+            }
+
         }
     }
 }
